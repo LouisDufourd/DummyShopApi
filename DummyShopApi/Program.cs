1672424259fc01338288690e466699f9f0189980
Louis DUFOURD <louisdufourd@pm.me>
2026-06-25 16:54:39 +0200

    feat(auth): configure JWT authentication

--- DummyShopApi/Program.cs
+++ DummyShopApi/Program.cs
@@ -3,7 +3,14 @@ using DummyShopApi.API.Filters;
 using DummyShopApi.BLL;
 using DummyShopApi.DAL;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
+using System.Text;
+
 
 namespace DummyShopApi
 {
@@ -29,8 +36,56 @@ namespace DummyShopApi
                 options.TypeDB = eDBType;
             });
 
+            builder.Services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidAudience = builder.Configuration["Jwt:Issuer"],
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(builder.Configuration["Jwt:Key"]!)),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen((options) =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath, true);
+
+                options.AddSecurityDefinition("jwt", new OpenApiSecurityScheme()
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Scheme = "Bearer",
+                    In = ParameterLocation.Header,
+                    Flows = new OpenApiOAuthFlows()
+                    {
+                        Password = new OpenApiOAuthFlow()
+                        {
+                            TokenUrl = new Uri("/api/authentification/loginSwagger", UriKind.Relative)
+                        }
+                    },
+                });
+
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "jwt" }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
             builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
             builder.Services.AddScoped<ValidationFilter>();
