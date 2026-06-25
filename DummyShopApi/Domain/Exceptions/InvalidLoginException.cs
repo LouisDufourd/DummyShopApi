a4f0cb56dd9e7e629513e245d08fea9786a5a069
Louis DUFOURD <louisdufourd@pm.me>
2026-06-25 12:06:01 +0200

    feat(auth): add authentication domain exceptions

---
+++ DummyShopApi/Domain/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,13 @@
+﻿namespace DummyShopApi.Domain.Exceptions
+{
+    public class InvalidLoginException : Exception
+    {
+        public InvalidLoginException()
+        {
+        }
+
+        public InvalidLoginException(string? message) : base(message)
+        {
+        }
+    }
+}
